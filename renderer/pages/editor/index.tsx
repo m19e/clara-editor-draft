b720d368885ff4d4a58dee72fe1a4688ab977c58
m19e <nexus.me7290@gmail.com>
2021-04-02 02:38:08 +0900

    Add draft editor demo page

---
+++ renderer/pages/editor/index.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import { Editor, EditorState } from "draft-js";
+import "draft-js/dist/Draft.css";
+
+const DraftEditor = () => {
+    const [editorState, setEditorState] = useState(() => EditorState.createEmpty());
+
+    return <Editor editorState={editorState} onChange={setEditorState} />;
+};
+
+export default DraftEditor;
