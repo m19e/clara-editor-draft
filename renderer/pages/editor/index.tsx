48f8566b5c1628b37a922d00b4c3efdd3cdcba32
m19e <nexus.me7290@gmail.com>
2021-04-02 22:33:09 +0900

    Vertical draft editor

--- renderer/pages/editor/index.tsx
+++ renderer/pages/editor/index.tsx
@@ -5,7 +5,11 @@ import "draft-js/dist/Draft.css";
 const DraftEditor = () => {
     const [editorState, setEditorState] = useState(() => EditorState.createEmpty());
 
-    return <Editor editorState={editorState} onChange={setEditorState} />;
+    return (
+        <div className="min-h-screen w-full flex justify-center items-center" style={{ writingMode: "vertical-rl" }}>
+            <Editor editorState={editorState} onChange={setEditorState} />
+        </div>
+    );
 };
 
 export default DraftEditor;
