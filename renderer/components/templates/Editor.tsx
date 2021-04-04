c7da0804ebc16eb35565cce6d6ef773270831758
m19e <nexus.me7290@gmail.com>
2021-04-04 16:32:04 +0900

    Set meta header editor page

--- renderer/components/templates/Editor.tsx
+++ renderer/components/templates/Editor.tsx
@@ -1,5 +1,11 @@
+import MetaHeader from "foundations/MetaHeader";
 import DraftEditor from "components/organisms/DraftEditor";
 
-const Editor = () => <DraftEditor />;
+const Editor = () => (
+    <>
+        <MetaHeader title="Clara Editor" />
+        <DraftEditor />
+    </>
+);
 
 export default Editor;
