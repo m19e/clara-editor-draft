e86a326aa125359d8105efa2626b2797e001c87a
m19e <nexus.me7290@gmail.com>
2021-04-07 00:55:32 +0900

    Set home button

--- renderer/components/organisms/DraftEditor.tsx
+++ renderer/components/organisms/DraftEditor.tsx
@@ -1,4 +1,5 @@
 import { remote } from "electron";
+import Link from "next/link";
 import { writeFileSync } from "fs";
 import { useState } from "react";
 import { ContentState, Editor, EditorState } from "draft-js";
@@ -44,6 +45,20 @@ const DraftEditor = () => {
                 <div>
                     <Editor editorState={editorState} onChange={setEditorState} />
                 </div>
+            </div>
+            <div className="absolute bottom-2 left-2 w-9 h-9 flex justify-center items-center bg-white rounded-full">
+                <Link href="/home">
+                    <a>
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-gray-900" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth={2}
+                                d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+                            />
+                        </svg>
+                    </a>
+                </Link>
             </div>
             <div className="absolute bottom-2 right-2 w-9 h-9 flex justify-center items-center bg-white rounded-full" onClick={() => saveDraft(editorState)}>
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-gray-900" fill="none" viewBox="0 0 24 24" stroke="currentColor">
