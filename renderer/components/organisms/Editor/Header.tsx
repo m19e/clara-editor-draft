41a83af049a6a0ca09391c62f3e4fa198725c883
m19e <nexus.me7290@gmail.com>
2021-04-10 03:39:40 +0900

    Set header skeleton

--- renderer/components/organisms/Editor/Header.tsx
+++ renderer/components/organisms/Editor/Header.tsx
@@ -1,5 +1,13 @@
 const Header = () => {
-    return <div></div>;
+    return (
+        <div className="fixed top-0 w-full h-24 flex-center shadow-sm editor-bg">
+            <div className="container flex justify-between items-center">
+                <div className="animate-pulse h-3 w-14 mt-1.5 mb-2 mx-0.5 bg-gray-300 rounded-sm"></div>
+                <div className="animate-pulse h-3 w-14 mt-1.5 mb-2 mx-0.5 bg-gray-300 rounded-sm"></div>
+                <div className="animate-pulse h-3 w-14 mt-1.5 mb-2 mx-0.5 bg-gray-300 rounded-sm"></div>
+            </div>
+        </div>
+    );
 };
 
 export default Header;
