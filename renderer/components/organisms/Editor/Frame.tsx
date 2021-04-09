8f4e5d918174e9502925b6ea540f2ce9f8c3b131
m19e <nexus.me7290@gmail.com>
2021-04-09 15:35:12 +0900

    Set header, footer in frame

--- renderer/components/organisms/Editor/Frame.tsx
+++ renderer/components/organisms/Editor/Frame.tsx
@@ -1,5 +1,13 @@
+import Header from "components/organisms/Editor/Header";
+import Footer from "components/organisms/Editor/Footer";
+
 const Frame = () => {
-    return <div></div>;
+    return (
+        <div>
+            <Header />
+            <Footer />
+        </div>
+    );
 };
 
 export default Frame;
