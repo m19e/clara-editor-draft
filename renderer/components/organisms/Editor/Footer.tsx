720f9fa644ee97359a573b71d30cbef52c1141b4
m19e <nexus.me7290@gmail.com>
2021-04-10 22:51:49 +0900

    Set control button skeleton

--- renderer/components/organisms/Editor/Footer.tsx
+++ renderer/components/organisms/Editor/Footer.tsx
@@ -1,12 +1,24 @@
 const Footer = () => {
     return (
-        <div className="fixed bottom-0 w-full shadow-2xl editor-bg">
+        <div className="fixed bottom-0 w-full shadow-2xl editor-bg text-gray-800">
             <div className="flex-center h-24 my-2">
-                <div className="animate-pulse h-3 w-14 mt-1.5 mb-2 mx-0.5 bg-gray-300 rounded-sm"></div>
+                <div className="flex-center flex-col">
+                    <button>↑</button>
+                    <div className="animate-pulse h-3 w-14 mt-1.5 mb-2 mx-0.5 bg-gray-300 rounded-sm"></div>
+                    <button>↓</button>
+                </div>
                 <span className="opacity-25 mx-3">・</span>
-                <div className="animate-pulse h-3 w-14 mt-1.5 mb-2 mx-0.5 bg-gray-300 rounded-sm"></div>
+                <div className="flex-center flex-col">
+                    <button>↑</button>
+                    <div className="animate-pulse h-3 w-14 mt-1.5 mb-2 mx-0.5 bg-gray-300 rounded-sm"></div>
+                    <button>↓</button>
+                </div>
                 <span className="opacity-25 mx-3">・</span>
-                <div className="animate-pulse h-3 w-14 mt-1.5 mb-2 mx-0.5 bg-gray-300 rounded-sm"></div>
+                <div className="flex-center flex-col">
+                    <button>↑</button>
+                    <div className="animate-pulse h-3 w-14 mt-1.5 mb-2 mx-0.5 bg-gray-300 rounded-sm"></div>
+                    <button>↓</button>
+                </div>
             </div>
         </div>
     );
