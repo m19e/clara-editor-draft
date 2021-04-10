b9b0955ec83afacfb44faf58bb20c794662d5b33
m19e <nexus.me7290@gmail.com>
2021-04-11 04:51:10 +0900

    Adjust spacing control box

--- renderer/components/organisms/Editor/Footer.tsx
+++ renderer/components/organisms/Editor/Footer.tsx
@@ -12,22 +12,30 @@ const Footer = () => {
     return (
         <div className="fixed bottom-0 w-full shadow-2xl editor-bg text-gray-800">
             <div className="flex-center h-24 my-2">
-                <div className="flex-center flex-col max-w-min">
+                <div className="flex-center flex-col w-20">
                     <button>↑</button>
-                    <span className="w-16 text-center">書体</span>
+                    <span className="text-center">書体</span>
                     <button>↓</button>
                 </div>
                 <span className="opacity-25 mx-3">・</span>
-                <div className="flex-center flex-col max-w-min">
-                    <button onClick={incFS}>↑</button>
-                    <span className="w-16 text-center">大きさ{fontSize}</span>
-                    <button onClick={decFS}>↓</button>
+                <div className="flex-center flex-col w-20">
+                    <button className="w-full" onClick={incFS}>
+                        ↑
+                    </button>
+                    <span className="w-full text-center">大きさ{fontSize}</span>
+                    <button className="w-full" onClick={decFS}>
+                        ↓
+                    </button>
                 </div>
                 <span className="opacity-25 mx-3">・</span>
-                <div className="flex-center flex-col max-w-min">
-                    <button onClick={incLW}>↑</button>
-                    <span className="w-16 text-center">字数{lineWords}</span>
-                    <button onClick={decLW}>↓</button>
+                <div className="flex-center flex-col w-20">
+                    <button className="w-full" onClick={incLW}>
+                        ↑
+                    </button>
+                    <span className="w-full text-center">字数{lineWords}</span>
+                    <button className="w-full" onClick={decLW}>
+                        ↓
+                    </button>
                 </div>
             </div>
         </div>
