436117292076cc42840b4fbbc0840383ed144c2b
m19e <nexus.me7290@gmail.com>
2021-04-11 04:27:41 +0900

    Adjust spacing control box

--- renderer/components/organisms/Editor/Footer.tsx
+++ renderer/components/organisms/Editor/Footer.tsx
@@ -12,21 +12,21 @@ const Footer = () => {
     return (
         <div className="fixed bottom-0 w-full shadow-2xl editor-bg text-gray-800">
             <div className="flex-center h-24 my-2">
-                <div className="flex-center flex-col">
+                <div className="flex-center flex-col max-w-min">
                     <button>↑</button>
-                    <span>書体</span>
+                    <span className="w-16 text-center">書体</span>
                     <button>↓</button>
                 </div>
                 <span className="opacity-25 mx-3">・</span>
-                <div className="flex-center flex-col">
+                <div className="flex-center flex-col max-w-min">
                     <button onClick={incFS}>↑</button>
-                    <span>大きさ{fontSize}</span>
+                    <span className="w-16 text-center">大きさ{fontSize}</span>
                     <button onClick={decFS}>↓</button>
                 </div>
                 <span className="opacity-25 mx-3">・</span>
-                <div className="flex-center flex-col">
+                <div className="flex-center flex-col max-w-min">
                     <button onClick={incLW}>↑</button>
-                    <span>字数{lineWords}</span>
+                    <span className="w-16 text-center">字数{lineWords}</span>
                     <button onClick={decLW}>↓</button>
                 </div>
             </div>
