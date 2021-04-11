6edd1b05316a8e109f1fc9f2c973c8b1361e5627
m19e <nexus.me7290@gmail.com>
2021-04-12 02:46:49 +0900

    Remove buttons

--- renderer/components/organisms/Editor/Footer.tsx
+++ renderer/components/organisms/Editor/Footer.tsx
@@ -13,29 +13,7 @@ const Footer = () => {
         <div className="fixed bottom-0 w-full shadow-2xl editor-bg text-gray-800">
             <div className="flex-center h-24 my-2">
                 <div className="flex-center flex-col w-20">
-                    <button>
-                        <span className="w-full flex-center opacity-50 hover:opacity-100">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="#2A2E3B">
-                                <path
-                                    fillRule="evenodd"
-                                    clipRule="evenodd"
-                                    d="M14.707 12.707a1 1 0 01-1.414 0L10 9.414l-3.293 3.293a1 1 0 01-1.414-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 010 1.414z"
-                                />
-                            </svg>
-                        </span>
-                    </button>
                     <span className="text-center">書体</span>
-                    <button>
-                        <span className="w-full flex-center opacity-50 hover:opacity-100">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="#2A2E3B">
-                                <path
-                                    fillRule="evenodd"
-                                    clipRule="evenodd"
-                                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                />
-                            </svg>
-                        </span>
-                    </button>
                 </div>
                 <span className="opacity-25 mx-3">・</span>
                 <div className="flex-center flex-col w-20 group">
