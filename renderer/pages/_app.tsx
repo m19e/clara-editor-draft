d14a2af1e1968ee7738c9a7df8501076e8a04c8d
m19e <nexus.me7290@gmail.com>
2021-04-12 19:22:23 +0900

    Wrap _app by recoil root

--- renderer/pages/_app.tsx
+++ renderer/pages/_app.tsx
@@ -1,6 +1,11 @@
 import type { AppProps } from "next/app";
+import { RecoilRoot } from "recoil";
 import "../styles/index.css";
 
-const App = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />;
+const App = ({ Component, pageProps }: AppProps) => (
+    <RecoilRoot>
+        <Component {...pageProps} />
+    </RecoilRoot>
+);
 
 export default App;
