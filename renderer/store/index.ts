30c809b995331229e1877d26fb12e8c96d0431e4
m19e <nexus.me7290@gmail.com>
2021-04-13 01:17:45 +0900

    Add real font-size selector

--- renderer/store/index.ts
+++ renderer/store/index.ts
@@ -1,4 +1,4 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
 import { FontType } from "types";
 
 export const fontTypeState = atom<FontType>({
@@ -9,4 +9,9 @@ export const fontTypeState = atom<FontType>({
 export const displayFontSizeState = atom({
     key: "editor/display-font-size",
     default: 7,
+});
+
+export const realFontSizeState = selector({
+    key: "editor/real-font-size",
+    get: ({ get }) => (get(displayFontSizeState) + 5) * 2,
 });
