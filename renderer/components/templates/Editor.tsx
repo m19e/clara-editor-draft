7d55b85f436a51fca53f96564e4bc0f5ab59515b
m19e <nexus.me7290@gmail.com>
2021-04-12 23:15:42 +0900

    Set entire page font

--- renderer/components/templates/Editor.tsx
+++ renderer/components/templates/Editor.tsx
@@ -1,13 +1,18 @@
 import MetaHeader from "foundations/MetaHeader";
 import DraftEditor from "components/organisms/DraftEditor";
 import Frame from "components/organisms/Editor/Frame";
+import { useFontType } from "hooks";
 
-const Editor = () => (
-    <div className="mincho">
-        <MetaHeader title="(タイトル) - Clara Editor" />
-        <DraftEditor />
-        <Frame />
-    </div>
-);
+const Editor = () => {
+    const [ft] = useFontType();
+
+    return (
+        <div className={ft}>
+            <MetaHeader title="(タイトル) - Clara Editor" />
+            <DraftEditor />
+            <Frame />
+        </div>
+    );
+};
 
 export default Editor;
