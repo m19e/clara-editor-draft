b89f3384cf5968af331cd211ec1a3c724aa86f2d
m19e <nexus.me7290@gmail.com>
2021-04-19 21:17:59 +0900

    Display router query

--- renderer/pages/editor/[draft]/index.tsx
+++ renderer/pages/editor/[draft]/index.tsx
@@ -3,7 +3,7 @@ import { useRouter } from "next/router";
 const Draft = () => {
     const router = useRouter();
 
-    return <pre>{JSON.stringify(router, null, 4)}</pre>;
+    return <pre>{JSON.stringify(router.query, null, 4)}</pre>;
 };
 
 export default Draft;
