57f760942283b51f9e95493238a6ed674e789a51
m19e <nexus.me7290@gmail.com>
2021-04-21 02:44:25 +0900

    Parse file name and set as title

--- renderer/pages/editor/[draft]/index.tsx
+++ renderer/pages/editor/[draft]/index.tsx
@@ -1,5 +1,6 @@
 import { useRouter } from "next/router";
 import { useState, useEffect } from "react";
+import { parse } from "path";
 import { readDraft } from "lib/draft";
 import { useFontType } from "hooks";
 import MetaHeader from "foundations/MetaHeader";
@@ -9,6 +10,7 @@ import Frame from "components/organisms/Editor/Frame";
 const Draft = () => {
     const router = useRouter();
     const [text, setText] = useState("");
+    const [title, setTitle] = useState("");
 
     const [ft] = useFontType();
 
@@ -17,6 +19,9 @@ const Draft = () => {
         if (router.route !== router.asPath && typeof draft === "string") {
             const data = readDraft(draft);
             setText(data);
+
+            const { name } = parse(draft);
+            setTitle(name);
         }
     }, [router]);
 
