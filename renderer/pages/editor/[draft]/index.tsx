7d501738f1eb461a71ad2454f0fbbc16981ecfbd
m19e <nexus.me7290@gmail.com>
2021-04-23 20:01:27 +0900

    Set format data from config

--- renderer/pages/editor/[draft]/index.tsx
+++ renderer/pages/editor/[draft]/index.tsx
@@ -2,7 +2,8 @@ import { useRouter } from "next/router";
 import { useState, useEffect } from "react";
 import { parse } from "path";
 import { readDraft } from "lib/draft";
-import { useFontType, useTitle } from "hooks";
+import { getFormat } from "lib/config";
+import { useFontType, useTitle, useFormat } from "hooks";
 import MetaHeader from "foundations/MetaHeader";
 import DraftEditor from "components/organisms/DraftEditor";
 import Frame from "components/organisms/Editor/Frame";
@@ -12,13 +13,16 @@ const Draft = () => {
     const [text, setText] = useState("");
     const [title, setTitle] = useTitle();
     const [ft] = useFontType();
+    const setFormat = useFormat();
 
     useEffect(() => {
         const { draft } = router.query;
         if (router.route !== router.asPath && typeof draft === "string") {
             const data = readDraft(draft);
             const { name } = parse(draft);
+            const [fontType, fontSize, lineWords] = getFormat();
 
+            setFormat({ fontType, fontSize, lineWords });
             setText(data);
             setTitle(name);
         }
