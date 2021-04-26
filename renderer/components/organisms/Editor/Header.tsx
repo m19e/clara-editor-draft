c7708199398f001e145fc45b4d7abecefacc6470
m19e <nexus.me7290@gmail.com>
2021-04-27 03:01:20 +0900

    Remove commented

--- renderer/components/organisms/Editor/Header.tsx
+++ renderer/components/organisms/Editor/Header.tsx
@@ -21,7 +21,6 @@ const Header = () => {
                         </a>
                     </Link>
                 </div>
-                {/* <div className="animate-pulse h-3 w-14 mt-1.5 mb-2 mx-0.5 bg-gray-300 rounded-sm"></div> */}
                 <span>{title}</span>
                 <div className="w-9 h-9 flex justify-center items-center transition-colors text-gray-600 hover:text-gray-900">
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
