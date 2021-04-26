9a91ab196b31876d1334b60fbf1c4883a61d1e5b
m19e <nexus.me7290@gmail.com>
2021-04-26 16:28:36 +0900

    Remove markup

--- renderer/pages/index.tsx
+++ renderer/pages/index.tsx
@@ -17,21 +17,6 @@ const Index = () => {
         <>
             <MetaHeader title="Index - Nextron (with-typescript-tailwindcss)" />
             <div>
-                <div className="grid grid-col-1 text-2xl w-full text-center">
-                    <img className="ml-auto mr-auto" src="/images/logo.png" />
-                    <span>⚡ Electron ⚡</span>
-                    <span>+</span>
-                    <span>Next.js</span>
-                    <span>+</span>
-                    <span>tailwindcss</span>
-                    <span>=</span>
-                    <span>💕 </span>
-                </div>
-                <div className="mt-1 w-full flex-wrap flex justify-center">
-                    <Link href="/editor">
-                        <a className="btn-blue">Go to editor page</a>
-                    </Link>
-                </div>
                 <div className="grid grid-col-1 w-full text-center">
                     {draftList.map((d, i) => {
                         const { name, ext } = parse(d);
