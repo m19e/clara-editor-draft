95849bfa0a7b8365259b73107ab8c555d1e0f6c6
m19e <nexus.me7290@gmail.com>
2021-05-02 15:08:53 +0900

    Add type handle selection state

--- renderer/types/index.ts
+++ renderer/types/index.ts
@@ -1,1 +1,9 @@
 export type FontType = "mincho" | "gothic";
+
+export type SelectionRangeOverride = {
+    anchorOffset: number;
+    focusOffset?: number;
+    anchorKey?: string;
+    focusKey?: string;
+    isBackward?: boolean;
+};
