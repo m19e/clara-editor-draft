50b3726102131522fa57f03d84c77365896dad20
m19e <nexus.me7290@gmail.com>
2021-05-04 18:50:58 +0900

    Replace by input-autosize

--- renderer/components/organisms/Editor/TitleEditForm.tsx
+++ renderer/components/organisms/Editor/TitleEditForm.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect, ChangeEvent, FormEvent } from "react";
+import AutosizeInput from "react-input-autosize";
 import { useTitle, useIsTitleEdit } from "hooks";
 import { renameDraft } from "lib/draft";
 
@@ -46,7 +47,7 @@ const TitleEditForm = () => {
         <>
             {isEdit ? (
                 <form onSubmit={handleStoreTitleChange}>
-                    <input
+                    {/* <input
                         className="text-gray-900 mx-2 text-center shadow-inner editor-bg rounded outline-none focus:outline-none"
                         type="text"
                         autoFocus
@@ -57,6 +58,18 @@ const TitleEditForm = () => {
                             if (e.key === "Tab") e.preventDefault();
                         }}
                         style={{ minWidth: "10rem", maxWidth: "50rem", width: `${localTitle.length + 1}rem` }}
+                    /> */}
+                    <AutosizeInput
+                        inputClassName="text-gray-900 px-2 text-center shadow-inner editor-bg rounded outline-none focus:outline-none"
+                        type="text"
+                        autoFocus
+                        value={localTitle}
+                        onChange={handleLocalTitleChange}
+                        onBlur={() => setIsEdit(false)}
+                        onKeyDown={(e) => {
+                            if (e.key === "Tab") e.preventDefault();
+                        }}
+                        inputStyle={{ minWidth: "10rem", maxWidth: "50rem" }}
                     />
                 </form>
             ) : (
