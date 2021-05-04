be6732aa369b193961f19071945f17f797a024cc
m19e <nexus.me7290@gmail.com>
2021-05-04 18:13:38 +0900

    Set home link icon

--- renderer/components/organisms/Editor/Header.tsx
+++ renderer/components/organisms/Editor/Header.tsx
@@ -4,18 +4,11 @@ import TitleEditForm from "components/organisms/Editor/TitleEditForm";
 const Header = () => {
     return (
         <div className="fixed top-0 w-full h-24 flex-center shadow-sm editor-bg">
-            <div className="container flex justify-between items-center">
-                <div className="w-9 h-9 flex justify-center items-center transition-colors text-gray-600 hover:text-gray-900">
+            <div className="w-11/12 sm:container flex justify-between items-center">
+                <div className="w-9 h-9 flex justify-start items-center">
                     <Link href="/">
                         <a>
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth={2}
-                                    d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
-                                />
-                            </svg>
+                            <img className="w-7 h-7" src="/images/icon-64x64.png" alt="Clara" />
                         </a>
                     </Link>
                 </div>
