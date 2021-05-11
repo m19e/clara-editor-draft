88b34c2e56d3260a702fd3ad80b78d7005953d34
m19e <nexus.me7290@gmail.com>
2021-05-11 18:24:54 +0900

    Fix spacing draft list

--- renderer/pages/index.tsx
+++ renderer/pages/index.tsx
@@ -89,27 +89,31 @@ const Index = () => {
                         </span>
                     </div>
                 </div>
-                <div className="grid grid-col-1 w-full text-center">
+                <div className="grid grid-col-1 w-full">
                     {draftList.map((d, i) => {
                         const { title, updated_at } = d;
                         const { name } = parse(title);
 
                         return (
-                            <div key={i} className="flex-center gap-4">
-                                <Link href={{ pathname: "/editor/[draft]", query: { draft: title } }}>
-                                    <a>{name}</a>
-                                </Link>
-                                <span>{getDisplayTime(updated_at)}</span>
-                                <span onClick={() => removeDraft(title)}>
-                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                        <path
-                                            strokeLinecap="round"
-                                            strokeLinejoin="round"
-                                            strokeWidth={2}
-                                            d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-                                        />
-                                    </svg>
-                                </span>
+                            <div className="flex-center">
+                                <div key={i} className="w-1/2 inline-flex justify-between gothic">
+                                    <Link href={{ pathname: "/editor/[draft]", query: { draft: title } }}>
+                                        <a>{name}</a>
+                                    </Link>
+                                    <div className="inline-flex gap-2">
+                                        <span>最終更新 {getDisplayTime(updated_at)}</span>
+                                        <span onClick={() => removeDraft(title)}>
+                                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                <path
+                                                    strokeLinecap="round"
+                                                    strokeLinejoin="round"
+                                                    strokeWidth={2}
+                                                    d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+                                                />
+                                            </svg>
+                                        </span>
+                                    </div>
+                                </div>
                             </div>
                         );
                     })}
