378620cc792bf07ee76c1fdbbc2c2f99c52473b3
m19e <nexus.me7290@gmail.com>
2021-05-12 03:50:25 +0900

    Text white

--- renderer/pages/index.tsx
+++ renderer/pages/index.tsx
@@ -97,7 +97,7 @@ const Index = () => {
                                     </svg>
                                 </button>
                                 <Link href={{ pathname: "/editor/[draft]", query: { draft: title } }}>
-                                    <a className="w-full p-2 inline-flex justify-between mincho group-hover:text-gray-50 group-hover:bg-gray-400 border-b border-gray-400">
+                                    <a className="w-full p-2 inline-flex justify-between mincho group-hover:text-white group-hover:bg-gray-400 border-b border-gray-400">
                                         <span>{name}</span>
                                         <div className="inline-flex justify-between" style={{ width: "7.75rem" }}>
                                             <span>{date}</span>
