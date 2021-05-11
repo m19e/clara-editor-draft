ac46084039615dcb20ffe1148d88eb0f90562a08
m19e <nexus.me7290@gmail.com>
2021-05-12 06:18:47 +0900

    Set perfect scrollbar

--- renderer/pages/index.tsx
+++ renderer/pages/index.tsx
@@ -2,6 +2,8 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 import { useState, useEffect } from "react";
 import { parse, extname } from "path";
+import Scrollbar from "react-perfect-scrollbar";
+import "react-perfect-scrollbar/dist/css/styles.css";
 import { initDraftDir, readDrafts, writeDraft, deleteDraft, getDraftStat } from "lib/draft";
 import MetaHeader from "foundations/MetaHeader";
 
@@ -70,57 +72,59 @@ const Index = () => {
     return (
         <>
             <MetaHeader title="一覧 - Clara Editor" />
-            <div className="min-h-screen flex-center flex-col">
-                <div className="grid grid-col-1 w-3/4">
-                    <div className="w-full inline-flex">
-                        <div className="w-7"></div>
-                        <div className="flex-1 inline-flex justify-between text mincho text-gray-600 px-2 pb-2 border-b border-gray-400">
-                            <span>原稿</span>
-                            <span className="text-left" style={{ width: "7.75rem" }}>
-                                最終更新
-                            </span>
-                        </div>
-                    </div>
-                    {draftList.map((d, i) => {
-                        const { title, updated_at } = d;
-                        const { name } = parse(title);
-                        const [date, time] = getDisplayTime(updated_at);
-
-                        return (
-                            <div key={i} className="flex-center text-gray-600 text-lg group">
-                                <button
-                                    className="pr-1 text-gray-600 opacity-0 group-hover:opacity-100 outline-none focus:outline-none"
-                                    onClick={() => removeDraft(title)}
-                                >
-                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
-                                    </svg>
-                                </button>
-                                <Link href={{ pathname: "/editor/[draft]", query: { draft: title } }}>
-                                    <a className="w-full px-2 py-4 inline-flex justify-between mincho group-hover:text-white group-hover:bg-gray-400 border-b border-gray-400">
-                                        <span>{name}</span>
-                                        <div className="inline-flex justify-between" style={{ width: "7.75rem" }}>
-                                            <span>{date}</span>
-                                            <span>{time}</span>
-                                        </div>
-                                    </a>
-                                </Link>
+            <Scrollbar className="max-h-screen">
+                <div className="min-h-screen flex-center flex-col">
+                    <div className="grid grid-col-1 w-3/4">
+                        <div className="w-full inline-flex">
+                            <div className="w-7"></div>
+                            <div className="flex-1 inline-flex justify-between text mincho text-gray-600 px-2 pb-2 border-b border-gray-400">
+                                <span>原稿</span>
+                                <span className="text-left" style={{ width: "7.75rem" }}>
+                                    最終更新
+                                </span>
                             </div>
-                        );
-                    })}
-                    <div className="inline-flex">
-                        <div className="w-7"></div>
-                        <div className="flex-1 flex-center">
-                            <button
-                                className="gothic rounded-full text-gray-700 border border-gray-400 px-6 py-1 mt-4 outline-none focus:outline-none"
-                                onClick={addDraft}
-                            >
-                                新しい原稿
-                            </button>
+                        </div>
+                        {draftList.map((d, i) => {
+                            const { title, updated_at } = d;
+                            const { name } = parse(title);
+                            const [date, time] = getDisplayTime(updated_at);
+
+                            return (
+                                <div key={i} className="flex-center text-gray-600 text-lg group">
+                                    <button
+                                        className="pr-1 text-gray-600 opacity-0 group-hover:opacity-100 outline-none focus:outline-none"
+                                        onClick={() => removeDraft(title)}
+                                    >
+                                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+                                        </svg>
+                                    </button>
+                                    <Link href={{ pathname: "/editor/[draft]", query: { draft: title } }}>
+                                        <a className="w-full px-2 py-4 inline-flex justify-between mincho group-hover:text-white group-hover:bg-gray-400 border-b border-gray-400">
+                                            <span>{name}</span>
+                                            <div className="inline-flex justify-between" style={{ width: "7.75rem" }}>
+                                                <span>{date}</span>
+                                                <span>{time}</span>
+                                            </div>
+                                        </a>
+                                    </Link>
+                                </div>
+                            );
+                        })}
+                        <div className="inline-flex">
+                            <div className="w-7"></div>
+                            <div className="flex-1 flex-center">
+                                <button
+                                    className="gothic rounded-full text-gray-700 border border-gray-400 px-6 py-1 mt-4 outline-none focus:outline-none"
+                                    onClick={addDraft}
+                                >
+                                    新しい原稿
+                                </button>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            </Scrollbar>
         </>
     );
 };
