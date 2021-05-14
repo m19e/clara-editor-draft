2c72e25baa371c365b0d0f3f44ac6e2dbb0c116c
m19e <nexus.me7290@gmail.com>
2021-05-14 17:21:32 +0900

    Fit updated_at header

--- renderer/pages/index.tsx
+++ renderer/pages/index.tsx
@@ -66,9 +66,7 @@ const Index = () => {
                         <div className="w-full grid grid-cols-12">
                             <div className="col-start-2 col-span-10 inline-flex justify-between text-sm mincho text-gray-600 px-4 pb-2 border-b border-gray-400">
                                 <span>原稿</span>
-                                <span className="text-left" style={{ width: "7.75rem" }}>
-                                    最終更新
-                                </span>
+                                <span className="text-left w-28">最終更新</span>
                             </div>
                         </div>
                         {draftList.map((draft, i) => (
