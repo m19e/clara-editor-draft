a684e6b2ae4eccbc1215017a7f6c9de689ce3977
m19e <nexus.me7290@gmail.com>
2021-05-19 08:46:36 +0900

    Responsive draft editor header

--- renderer/components/organisms/Editor/Header.tsx
+++ renderer/components/organisms/Editor/Header.tsx
@@ -4,16 +4,18 @@ import TitleEditForm from "components/organisms/Editor/TitleEditForm";
 const Header = () => {
     return (
         <div className="fixed top-0 w-full h-20 flex-center shadow-sm editor-bg">
-            <div className="w-11/12 sm:container flex justify-between items-center">
-                <div className="w-9 h-9 flex justify-start items-center">
-                    <Link href="/">
-                        <a>
-                            <img className="w-7 h-7" src="/images/icon-64x64.png" alt="Home" />
-                        </a>
-                    </Link>
+            <div className="container flex-center">
+                <div className="w-11/12 xl:max-w-4xl flex justify-between items-center">
+                    <div className="w-9 h-9 flex justify-start items-center">
+                        <Link href="/">
+                            <a>
+                                <img className="w-7 h-7" src="/images/icon-64x64.png" alt="Home" />
+                            </a>
+                        </Link>
+                    </div>
+                    <TitleEditForm />
+                    <div className="w-9"></div>
                 </div>
-                <TitleEditForm />
-                <div className="w-9"></div>
             </div>
         </div>
     );
