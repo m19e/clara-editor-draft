3a2f402e5be70416d0fb995c36e1df9abf4b74c5
m19e <nexus.me7290@gmail.com>
2021-05-19 08:29:10 +0900

    Adjust create button margin-top

--- renderer/pages/index.tsx
+++ renderer/pages/index.tsx
@@ -75,7 +75,7 @@ const Index = () => {
                         ))}
                         <div className="flex-center">
                             <button
-                                className="gothic rounded-full text-gray-700 border border-gray-500 px-6 py-1 mt-4 outline-none focus:outline-none"
+                                className="gothic rounded-full text-gray-700 border border-gray-500 px-6 py-1 mt-8 outline-none focus:outline-none"
                                 onClick={addDraft}
                             >
                                 新しい原稿
