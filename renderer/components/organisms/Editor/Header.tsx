919d64b6a2ebe2e42ea3d6d513d0feadd52d067f
m19e <nexus.me7290@gmail.com>
2021-05-19 09:06:23 +0900

    Justify-end home icon

--- renderer/components/organisms/Editor/Header.tsx
+++ renderer/components/organisms/Editor/Header.tsx
@@ -6,14 +6,14 @@ const Header = () => {
         <div className="fixed top-0 w-full h-20 flex-center shadow-sm editor-bg">
             <div className="container flex-center">
                 <div className="w-11/12 xl:max-w-4xl grid grid-cols-12 items-center">
-                    <div className="col-span-1">
+                    <div className="col-span-1 inline-flex justify-end">
                         <Link href="/">
                             <a>
                                 <img className="w-7 h-7" src="/images/icon-64x64.png" alt="Home" />
                             </a>
                         </Link>
                     </div>
-                    <div className="col-span-10 flex-center inline-flex">
+                    <div className="col-span-10 flex-center">
                         <TitleEditForm />
                     </div>
                     <div className="w-9"></div>
