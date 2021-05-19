7af83fcc8cd6a15c0b4cb2c51905577f200cf38e
m19e <nexus.me7290@gmail.com>
2021-05-19 09:03:59 +0900

    Replace by grid

--- renderer/components/organisms/Editor/Header.tsx
+++ renderer/components/organisms/Editor/Header.tsx
@@ -5,15 +5,17 @@ const Header = () => {
     return (
         <div className="fixed top-0 w-full h-20 flex-center shadow-sm editor-bg">
             <div className="container flex-center">
-                <div className="w-11/12 xl:max-w-4xl flex justify-between items-center">
-                    <div className="w-9 h-9 flex justify-start items-center">
+                <div className="w-11/12 xl:max-w-4xl grid grid-cols-12 items-center">
+                    <div className="col-span-1">
                         <Link href="/">
                             <a>
                                 <img className="w-7 h-7" src="/images/icon-64x64.png" alt="Home" />
                             </a>
                         </Link>
                     </div>
-                    <TitleEditForm />
+                    <div className="col-span-10 flex-center inline-flex">
+                        <TitleEditForm />
+                    </div>
                     <div className="w-9"></div>
                 </div>
             </div>
