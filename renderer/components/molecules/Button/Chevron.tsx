7d5618f56f01cc14b7574835ca9c17b756bedddf
m19e <nexus.me7290@gmail.com>
2021-05-19 09:47:05 +0900

    Remove class from chevron

--- renderer/components/molecules/Button/Chevron.tsx
+++ renderer/components/molecules/Button/Chevron.tsx
@@ -5,11 +5,7 @@ type Props = {
 };
 
 const Chevron = ({ type, onClick, disabled }: Props) => (
-    <button
-        className="outline-none focus:outline-none transition-opacity duration-1000 ease-out opacity-0 group-hover:opacity-75"
-        onClick={onClick}
-        disabled={disabled}
-    >
+    <button className="transition-opacity duration-1000 ease-out opacity-0 group-hover:opacity-75" onClick={onClick} disabled={disabled}>
         <span className={"flex-center " + (disabled ? "opacity-25" : "opacity-50 hover:opacity-100")}>
             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="#2A2E3B">
                 <path
