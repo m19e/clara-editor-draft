ba3a2e13ed619652173c1d2d984b37c5f1988609
m19e <nexus.me7290@gmail.com>
2021-05-23 14:15:07 +0900

    Set autosave duration in draft page init process

--- renderer/pages/editor/[draft]/index.tsx
+++ renderer/pages/editor/[draft]/index.tsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 import { parse } from "path";
 import { readDraft } from "lib/draft";
 import { getFormat } from "lib/config";
-import { useFontType, useTitle, useFormat } from "hooks";
+import { useFontType, useTitle, useFormat, useAutosaveDuration } from "hooks";
 import DraftEditor from "components/organisms/DraftEditor";
 import Frame from "components/organisms/Editor/Frame";
 
@@ -13,15 +13,17 @@ const Draft = () => {
     const [, setTitle] = useTitle();
     const [ft] = useFontType();
     const setFormat = useFormat();
+    const [, setAutosaveDuration] = useAutosaveDuration();
 
     useEffect(() => {
         const { draft } = router.query;
         if (router.route !== router.asPath && typeof draft === "string") {
             const data = readDraft(draft);
             const { name } = parse(draft);
-            const [fontType, fontSize, lineWords] = getFormat();
+            const [fontType, fontSize, lineWords, autosaveDuration] = getFormat();
 
             setFormat({ fontType, fontSize, lineWords });
+            setAutosaveDuration(autosaveDuration);
             setText(data);
             setTitle(name);
         }
