5b43cfa622e9318333ad26543398b46e6c556ddf
m19e <nexus.me7290@gmail.com>
2021-05-28 09:05:05 +0900

    Remove demo

--- renderer/pages/_app.tsx
+++ renderer/pages/_app.tsx
@@ -1,37 +1,11 @@
 import type { AppProps } from "next/app";
 import { RecoilRoot } from "recoil";
-import { remote } from "electron";
 import "../styles/index.css";
 
-const App = ({ Component, pageProps }: AppProps) => {
-    const remoteMenu = remote.Menu;
-    const localMenu = remoteMenu.buildFromTemplate([
-        {
-            label: "設定",
-            submenu: [
-                {
-                    id: "char-count-display",
-                    label: "文字数を表示する",
-                    type: "checkbox",
-                    accelerator: "CmdOrCtrl+T",
-                    checked: true,
-                    click: (self, focusedWin) => {
-                        if (focusedWin) {
-                            // setDisplayCharCount(self.checked)
-                        }
-                    },
-                },
-            ],
-        },
-    ]);
-
-    remoteMenu.setApplicationMenu(localMenu);
-
-    return (
-        <RecoilRoot>
-            <Component {...pageProps} />
-        </RecoilRoot>
-    );
-};
+const App = ({ Component, pageProps }: AppProps) => (
+    <RecoilRoot>
+        <Component {...pageProps} />
+    </RecoilRoot>
+);
 
 export default App;
