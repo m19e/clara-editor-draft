248190c8e5c1dac77c52f69ef61a1d9859ebf869
m19e <nexus.me7290@gmail.com>
2021-05-28 09:26:37 +0900

    Initialize menu

--- renderer/pages/editor/[draft]/index.tsx
+++ renderer/pages/editor/[draft]/index.tsx
@@ -1,9 +1,10 @@
+import { remote } from "electron";
 import { useRouter } from "next/router";
 import { useState, useEffect } from "react";
 import { parse } from "path";
 import { readDraft } from "lib/draft";
 import { getFormat } from "lib/config";
-import { useFontType, useTitle, useFormat, useAutosaveDuration } from "hooks";
+import { useFontType, useTitle, useFormat, useAutosaveDuration, useDisplayCharCount } from "hooks";
 import DraftEditor from "components/organisms/DraftEditor";
 import Frame from "components/organisms/Editor/Frame";
 
@@ -15,7 +16,31 @@ const Draft = () => {
     const setFormat = useFormat();
     const [, setAutosaveDuration] = useAutosaveDuration();
 
+    const [, setDisplayCharCount] = useDisplayCharCount();
+    const remoteMenu = remote.Menu;
+
     useEffect(() => {
+        const localMenu = remoteMenu.buildFromTemplate([
+            {
+                label: "設定",
+                submenu: [
+                    {
+                        id: "char-count-display",
+                        label: "文字数を表示する",
+                        type: "checkbox",
+                        accelerator: "CmdOrCtrl+T",
+                        checked: true,
+                        click: (self, focusedWin) => {
+                            if (focusedWin) {
+                                setDisplayCharCount(self.checked);
+                            }
+                        },
+                    },
+                ],
+            },
+        ]);
+        remoteMenu.setApplicationMenu(localMenu);
+
         const { draft } = router.query;
         if (router.route !== router.asPath && typeof draft === "string") {
             const data = readDraft(draft);
