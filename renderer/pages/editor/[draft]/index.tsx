b823b7ac9811a4161e1a1ef10282c69bc6f83be4
m19e <nexus.me7290@gmail.com>
2021-06-01 21:30:15 +0900

    Shift menu initialize process to fund

--- renderer/pages/editor/[draft]/index.tsx
+++ renderer/pages/editor/[draft]/index.tsx
@@ -5,6 +5,7 @@ import { parse } from "path";
 import { readDraft } from "lib/draft";
 import { getFormat } from "lib/config";
 import { useFontType, useTitle, useFormat, useAutosaveDuration, useDisplayCharCount } from "hooks";
+import Menu from "foundations/Menu";
 import DraftEditor from "components/organisms/DraftEditor";
 import Frame from "components/organisms/Editor/Frame";
 
@@ -17,70 +18,70 @@ const Draft = () => {
     const [autosaveDuration, setAutosaveDuration] = useAutosaveDuration();
     const [, setDisplayCharCount] = useDisplayCharCount();
 
-    useEffect(() => {
-        const localMenu = remote.Menu.buildFromTemplate([
-            {
-                label: "設定",
-                submenu: [
-                    {
-                        id: "char-count-display",
-                        label: "文字数を表示する",
-                        type: "checkbox",
-                        accelerator: "CmdOrCtrl+T",
-                        checked: true,
-                        click: (self, focusedWin) => {
-                            if (focusedWin) {
-                                setDisplayCharCount(self.checked);
-                            }
-                        },
-                    },
-                    {
-                        id: "autosave-duration",
-                        label: "自動保存",
-                        submenu: [
-                            {
-                                id: "duration-1-sec",
-                                label: "1秒",
-                                type: "checkbox",
-                                checked: autosaveDuration === 1,
-                                enabled: autosaveDuration !== 1,
-                                click: (_, focusedWin) => {
-                                    if (focusedWin) {
-                                        setAutosaveDuration(1);
-                                    }
-                                },
-                            },
-                            {
-                                id: "duration-5-sec",
-                                label: "5秒",
-                                type: "checkbox",
-                                checked: autosaveDuration === 5,
-                                enabled: autosaveDuration !== 5,
-                                click: (_, focusedWin) => {
-                                    if (focusedWin) {
-                                        setAutosaveDuration(5);
-                                    }
-                                },
-                            },
-                            {
-                                id: "duration-10-sec",
-                                label: "10秒",
-                                type: "checkbox",
-                                checked: autosaveDuration === 10,
-                                enabled: autosaveDuration !== 10,
-                                click: (_, focusedWin) => {
-                                    if (focusedWin) {
-                                        setAutosaveDuration(10);
-                                    }
-                                },
-                            },
-                        ],
-                    },
-                ],
-            },
-        ]);
-        remote.Menu.setApplicationMenu(localMenu);
-    }, [autosaveDuration]);
+    // useEffect(() => {
+    //     const localMenu = remote.Menu.buildFromTemplate([
+    //         {
+    //             label: "設定",
+    //             submenu: [
+    //                 {
+    //                     id: "char-count-display",
+    //                     label: "文字数を表示する",
+    //                     type: "checkbox",
+    //                     accelerator: "CmdOrCtrl+T",
+    //                     checked: true,
+    //                     click: (self, focusedWin) => {
+    //                         if (focusedWin) {
+    //                             setDisplayCharCount(self.checked);
+    //                         }
+    //                     },
+    //                 },
+    //                 {
+    //                     id: "autosave-duration",
+    //                     label: "自動保存",
+    //                     submenu: [
+    //                         {
+    //                             id: "duration-1-sec",
+    //                             label: "1秒",
+    //                             type: "checkbox",
+    //                             checked: autosaveDuration === 1,
+    //                             enabled: autosaveDuration !== 1,
+    //                             click: (_, focusedWin) => {
+    //                                 if (focusedWin) {
+    //                                     setAutosaveDuration(1);
+    //                                 }
+    //                             },
+    //                         },
+    //                         {
+    //                             id: "duration-5-sec",
+    //                             label: "5秒",
+    //                             type: "checkbox",
+    //                             checked: autosaveDuration === 5,
+    //                             enabled: autosaveDuration !== 5,
+    //                             click: (_, focusedWin) => {
+    //                                 if (focusedWin) {
+    //                                     setAutosaveDuration(5);
+    //                                 }
+    //                             },
+    //                         },
+    //                         {
+    //                             id: "duration-10-sec",
+    //                             label: "10秒",
+    //                             type: "checkbox",
+    //                             checked: autosaveDuration === 10,
+    //                             enabled: autosaveDuration !== 10,
+    //                             click: (_, focusedWin) => {
+    //                                 if (focusedWin) {
+    //                                     setAutosaveDuration(10);
+    //                                 }
+    //                             },
+    //                         },
+    //                     ],
+    //                 },
+    //             ],
+    //         },
+    //     ]);
+    //     remote.Menu.setApplicationMenu(localMenu);
+    // }, [autosaveDuration]);
 
     useEffect(() => {
         const { draft } = router.query;
@@ -98,6 +99,7 @@ const Draft = () => {
 
     return (
         <div className={ft + " text-black editor-bg"}>
+            <Menu />
             <DraftEditor text={text} />
             <Frame />
         </div>
