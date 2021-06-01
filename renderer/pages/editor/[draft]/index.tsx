5acf537b5fb61e185d8a686769daa1252e7d6fe1
m19e <nexus.me7290@gmail.com>
2021-06-01 21:33:28 +0900

    Remove needless hook

--- renderer/pages/editor/[draft]/index.tsx
+++ renderer/pages/editor/[draft]/index.tsx
@@ -15,73 +15,7 @@ const Draft = () => {
     const [, setTitle] = useTitle();
     const [ft] = useFontType();
     const setFormat = useFormat();
-    const [autosaveDuration, setAutosaveDuration] = useAutosaveDuration();
-    const [, setDisplayCharCount] = useDisplayCharCount();
-
-    // useEffect(() => {
-    //     const localMenu = remote.Menu.buildFromTemplate([
-    //         {
-    //             label: "設定",
-    //             submenu: [
-    //                 {
-    //                     id: "char-count-display",
-    //                     label: "文字数を表示する",
-    //                     type: "checkbox",
-    //                     accelerator: "CmdOrCtrl+T",
-    //                     checked: true,
-    //                     click: (self, focusedWin) => {
-    //                         if (focusedWin) {
-    //                             setDisplayCharCount(self.checked);
-    //                         }
-    //                     },
-    //                 },
-    //                 {
-    //                     id: "autosave-duration",
-    //                     label: "自動保存",
-    //                     submenu: [
-    //                         {
-    //                             id: "duration-1-sec",
-    //                             label: "1秒",
-    //                             type: "checkbox",
-    //                             checked: autosaveDuration === 1,
-    //                             enabled: autosaveDuration !== 1,
-    //                             click: (_, focusedWin) => {
-    //                                 if (focusedWin) {
-    //                                     setAutosaveDuration(1);
-    //                                 }
-    //                             },
-    //                         },
-    //                         {
-    //                             id: "duration-5-sec",
-    //                             label: "5秒",
-    //                             type: "checkbox",
-    //                             checked: autosaveDuration === 5,
-    //                             enabled: autosaveDuration !== 5,
-    //                             click: (_, focusedWin) => {
-    //                                 if (focusedWin) {
-    //                                     setAutosaveDuration(5);
-    //                                 }
-    //                             },
-    //                         },
-    //                         {
-    //                             id: "duration-10-sec",
-    //                             label: "10秒",
-    //                             type: "checkbox",
-    //                             checked: autosaveDuration === 10,
-    //                             enabled: autosaveDuration !== 10,
-    //                             click: (_, focusedWin) => {
-    //                                 if (focusedWin) {
-    //                                     setAutosaveDuration(10);
-    //                                 }
-    //                             },
-    //                         },
-    //                     ],
-    //                 },
-    //             ],
-    //         },
-    //     ]);
-    //     remote.Menu.setApplicationMenu(localMenu);
-    // }, [autosaveDuration]);
+    const [, setAutosaveDuration] = useAutosaveDuration();
 
     useEffect(() => {
         const { draft } = router.query;
