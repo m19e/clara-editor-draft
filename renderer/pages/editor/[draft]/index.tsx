33f44513f5e2fb5aeb81cc1b794bd1134a134fd4
m19e <nexus.me7290@gmail.com>
2021-06-01 22:15:04 +0900

    Remove import

--- renderer/pages/editor/[draft]/index.tsx
+++ renderer/pages/editor/[draft]/index.tsx
@@ -1,10 +1,9 @@
-import { remote } from "electron";
 import { useRouter } from "next/router";
 import { useState, useEffect } from "react";
 import { parse } from "path";
 import { readDraft } from "lib/draft";
 import { getFormat } from "lib/config";
-import { useFontType, useTitle, useFormat, useAutosaveDuration, useDisplayCharCount } from "hooks";
+import { useFontType, useTitle, useFormat, useAutosaveDuration } from "hooks";
 import Menu from "foundations/Menu";
 import DraftEditor from "components/organisms/DraftEditor";
 import Frame from "components/organisms/Editor/Frame";
