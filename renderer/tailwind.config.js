57d41fdf82e33d049418ea2cbd0635d4301aca3b
m19e <nexus.me7290@gmail.com>
2021-06-02 19:27:23 +0900

    Add darkgray color in tailwind config

--- renderer/tailwind.config.js
+++ renderer/tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
     theme: {
         extend: {
             colors: {
-                "accent-1": "#333",
+                darkgray: "#44454a",
             },
         },
     },
