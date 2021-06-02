1600c3749739a02e1d11bfb7936778b68561c4cb
m19e <nexus.me7290@gmail.com>
2021-06-02 20:00:19 +0900

    Provide theme to app

--- renderer/pages/_app.tsx
+++ renderer/pages/_app.tsx
@@ -1,11 +1,14 @@
 import type { AppProps } from "next/app";
+import { ThemeProvider } from "next-themes";
 import { RecoilRoot } from "recoil";
 import "../styles/index.css";
 
 const App = ({ Component, pageProps }: AppProps) => (
-    <RecoilRoot>
-        <Component {...pageProps} />
-    </RecoilRoot>
+    <ThemeProvider attribute="class">
+        <RecoilRoot>
+            <Component {...pageProps} />
+        </RecoilRoot>
+    </ThemeProvider>
 );
 
 export default App;
