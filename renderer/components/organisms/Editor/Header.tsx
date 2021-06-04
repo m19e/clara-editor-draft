3dcbc42284265fe6470e972782ff0fda4dfc848e
m19e <nexus.me7290@gmail.com>
2021-06-05 07:03:45 +0900

    Adjust header height

--- renderer/components/organisms/Editor/Header.tsx
+++ renderer/components/organisms/Editor/Header.tsx
@@ -6,7 +6,7 @@ const Header = () => {
     const { theme } = useTheme();
 
     return (
-        <div className={"fixed top-0 w-full h-20 flex-center select-none " + (theme === "dark" ? "shadow clara-bg__dark" : "shadow-sm clara-bg__light")}>
+        <div className={"fixed top-0 w-full h-16 flex-center select-none " + (theme === "dark" ? "shadow clara-bg__dark" : "shadow-sm clara-bg__light")}>
             <div className="container flex-center">
                 <div className="w-11/12 xl:max-w-4xl grid grid-cols-12 items-center">
                     <div className="col-span-1 inline-flex justify-end">
