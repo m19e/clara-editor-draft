886fa7d13c69456a7733cfdcd7a3f5aa46a19af0
m19e <nexus.me7290@gmail.com>
2021-06-05 07:00:37 +0900

    Chevron current color

--- renderer/components/molecules/Button/Chevron.tsx
+++ renderer/components/molecules/Button/Chevron.tsx
@@ -7,7 +7,7 @@ type Props = {
 const Chevron = ({ type, onClick, disabled }: Props) => (
     <button className="transition-opacity duration-1000 ease-out opacity-0 group-hover:opacity-75" onClick={onClick} disabled={disabled}>
         <span className={"flex-center " + (disabled ? "opacity-25" : "opacity-50 hover:opacity-100")}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="#2A2E3B">
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="currentColor">
                 <path
                     fillRule="evenodd"
                     clipRule="evenodd"
